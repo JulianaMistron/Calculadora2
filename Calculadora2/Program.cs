f0631fcdee390801a2ff175c020bcef749961587
JulianaMistron <jumistron@yahoo.com.br>
2024-04-19 10:19:51 -0300

    primeiro commit

--- Calculadora2/Program.cs
+++ Calculadora2/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿float n1=0, n2=0, n3=0, n4=0, resultado;
+int opcao;
+
+Console.WriteLine("Informe o primeiro valor: ");
+n1 = float.Parse(Console.ReadLine());
+
+while(n2 == 0)
+{
+    Console.WriteLine("Informe o segundo valor: ");
+    n2 = float.Parse(Console.ReadLine());
+}
+
+do
+{
+    Console.WriteLine("Informe o terceiro valor: ");
+    n3 = float.Parse(Console.ReadLine());
+
+}while(n3 == 0);
+
+for (n4 = 0; n4 == 0; )
+{
+    Console.WriteLine("Informe o quarto valor: ");
+    n4 = float.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Informe a operação desejada: ");
+Console.WriteLine("1 - Adição: ");
+Console.WriteLine("2 - Subtração: ");
+Console.WriteLine("3 - Multiplicação: ");
+Console.WriteLine("4 - Divisão: ");
+opcao = int.Parse(Console.ReadLine());
+
+if(opcao == 1)
+{
+   resultado =  n1 + n2 + n3 + n4;
+   Console.WriteLine("O resultado é: "+resultado);
+}
+else
+{
+    if(opcao == 2)
+    {
+        resultado = n1 - n2 - n3 - n4;
+        Console.WriteLine($"O resultado é:  {resultado}");
+    }
+    else
+    {
+        if(opcao == 3)
+        {
+            resultado = n1 * n2 * n3 * n4;
+            Console.WriteLine("O resultado é: " + resultado);
+        }
+        else
+        {
+            resultado = n1 / n2 / n3 / n4;
+            Console.WriteLine($"O resultado é:  {resultado}");
+        }
+    }
+}
+
+
